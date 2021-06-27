e07801f99b7a76575bca55b671e204b4fd49b011
Schadough <schadough@gmail.com>
2021-06-27 16:19:08 -0500

    boostrap form init

---
+++ src/views/SignUp.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container, Row, Form } from "react-bootstrap";
+import { withFormik } from "formik";
+
+export const SignUp = () => {
+  return (
+    <>
+      <Container>
+        <Form></Form>
+      </Container>
+    </>
+  );
+};
